7b5e05c2853bf460ef839e443f35c0b33123a955
muhdnihal <muhdnihal132@gmail.com>
2024-09-24 21:02:02 +0530

    register page

--- baby-products/src/pages/Register.jsx
+++ baby-products/src/pages/Register.jsx
@@ -1,20 +1,50 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Register = () => {
+  const [inputs,setInputs] =useState({
+    username: '',
+    email: '',
+    password: '',
+    cpassword: ''
+  })
+  const handleChange =(e)=>{
+    const name = e.target.name;
+    const value = e.target.value;
+     setInputs({...inputs,[inputs]: value})
+  }
   return (
     <section>
       <h1>Register</h1>
-      <form action="">
+      <form onSubmit={}>
         <label htmlFor="username">Username:</label>
         <input   
              type="text"
-             id='username'
-             placeholder='Enter Your Username'    
+             name='username'
+             placeholder='Username'
+             value={inputs.username}
+             onChange={handleChange}    
                  />
+        <label htmlFor="">Email:</label>
+        <input 
+                type="email"
+                name='email'
+                placeholder='Email'
+                value={inputs.email}
+                onChange={handleChange} />
         <label htmlFor="">Password:</label>
-        <input type="text" />
+        <input 
+                type="text"
+                name='password'
+                placeholder='Password'
+                value={inputs.password}
+                onChange={handleChange} />
         <label htmlFor="">Confirm Password</label>
-        <input type="text" />
+        <input 
+                type="text"
+                name='cpassword'
+                placeholder='Confirm Password'
+                value={inputs.password}
+                onChange={handleChange} />
         <button onClick>Sign Up</button>
       </form>
     </section>
