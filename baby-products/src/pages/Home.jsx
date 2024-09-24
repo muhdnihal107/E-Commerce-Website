5de04f56a08855e105e1b753c52de8975aaa31ed
muhdnihal <muhdnihal132@gmail.com>
2024-09-24 13:07:51 +0530

    css change

--- baby-products/src/pages/Home.jsx
+++ baby-products/src/pages/Home.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div className='home-head-con'>
+        <div></div>
+        <div>
+          <h1>Baby Essential</h1>
+          </div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className='home-img-con'>
+        <img src="" alt="" />
+      </div>
+    </div>
   )
 }
 
