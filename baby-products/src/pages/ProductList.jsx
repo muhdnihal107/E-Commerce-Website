6d8eb53a0c7deedecc6692d9275faa42b6b8aeb2
muhdnihal <muhdnihal132@gmail.com>
2024-09-26 15:49:31 +0530

    install json server and created database for my web

--- baby-products/src/pages/ProductList.jsx
+++ baby-products/src/pages/ProductList.jsx
@@ -2,7 +2,49 @@ import React from 'react'
 
 const ProductList = () => {
   return (
-    <div>ProductList</div>
+    <div>
+      <div>
+             <h1>Shop</h1>
+      </div>
+      <div>
+        <div>
+          <div><img src="" alt="" /></div>
+          <div></div>
+        </div>
+        <div>
+          <div><img src="" alt="" /></div>
+          <div></div>
+        </div>
+        <div>
+          <div><img src="" alt="" /></div>
+          <div></div>
+        </div>
+        <div>
+          <div><img src="" alt="" /></div>
+          <div></div>
+        </div>
+        <div>
+          <div><img src="" alt="" /></div>
+          <div></div>
+        </div>
+        <div>
+          <div><img src="" alt="" /></div>
+          <div></div>
+        </div>
+        <div>
+          <div><img src="" alt="" /></div>
+          <div></div>
+        </div>
+        <div>
+          <div><img src="" alt="" /></div>
+          <div></div>
+        </div>
+        <div>
+          <div><img src="" alt="" /></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
