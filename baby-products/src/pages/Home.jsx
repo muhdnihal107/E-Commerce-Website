5eb5ee110e4d5944fdde06643b223118c4ac4a10
muhdnihal <muhdnihal132@gmail.com>
2025-02-01 14:10:54 +0530

    edit2

--- baby-products/src/pages/Home.jsx
+++ baby-products/src/pages/Home.jsx
@@ -43,7 +43,7 @@ const Home = () => {
     </div>
     <div className="md:w-1/2 mt-10 md:mt-0">
       <img
-        src="src/assets/homecover.jpg"
+        src="https://ibb.co/fd3zD5K9"
         alt="Baby Products"
         className="w-full h-96 object-cover rounded-lg shadow-2xl transform hover:scale-105 transition duration-300"
       />
